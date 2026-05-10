eb54d15747834e202d2e64023cde096abbe9edd8
Yerdaulet <yerdauletacc@gmail.com>
2026-05-10 14:54:44 +0500

    initial commit

---
+++ src/main/java/com/example/englishtrainer/EnglishTrainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.englishtrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishTrainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishTrainerApplication.class, args);
+    }
+
+}
